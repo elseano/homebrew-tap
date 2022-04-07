72dc79391d337c68c39199293e6e41a6d4c61a71
Sean St. Quentin <sean.stquentin@gmail.com>
2022-04-07 12:08:23 +1000

    Brew formula update for rundown version v0.4.0-beta.16

--- Formula/rundown.rb
+++ Formula/rundown.rb
@@ -5,25 +5,25 @@
 class Rundown < Formula
   desc "Rundown runs Markdown files, making them powerful and pretty console applications"
   homepage "https://github.com/elseano/rundown"
-  version "0.4.0-beta.15"
+  version "0.4.0-beta.16"
   license "MIT"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/elseano/rundown/releases/download/v0.4.0-beta.15/rundown_0.4.0-beta.15_Darwin_x86_64.tar.gz"
-    sha256 "10074638a9ee79983ec861a45c1ee368ca2803b4fcd30ee6b85b6048f463948b"
+    url "https://github.com/elseano/rundown/releases/download/v0.4.0-beta.16/rundown_0.4.0-beta.16_Darwin_x86_64.tar.gz"
+    sha256 "d8f8329334d269436458a72def15ca1598f41339619a24c5d764fc96f6af6a71"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/elseano/rundown/releases/download/v0.4.0-beta.15/rundown_0.4.0-beta.15_Darwin_arm64.tar.gz"
-    sha256 "59cb9fef7f917c7c8bea68cd8ffc1d89172054dd054807c9c5971e50bd104fc6"
+    url "https://github.com/elseano/rundown/releases/download/v0.4.0-beta.16/rundown_0.4.0-beta.16_Darwin_arm64.tar.gz"
+    sha256 "9f64e6b7fa7c8977372ca79ceb634bf58af104e061aac812195ae3b9686a67d1"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/elseano/rundown/releases/download/v0.4.0-beta.15/rundown_0.4.0-beta.15_Linux_x86_64.tar.gz"
-    sha256 "79d0384cb39fcb143516d390d90069bec2702644e2188c29ddcf1febb31ec326"
+    url "https://github.com/elseano/rundown/releases/download/v0.4.0-beta.16/rundown_0.4.0-beta.16_Linux_x86_64.tar.gz"
+    sha256 "d045f73c93b38ba4ece1d9b48bc0eb42f007be7c0a0dc014d63b0d9f43f86b16"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/elseano/rundown/releases/download/v0.4.0-beta.15/rundown_0.4.0-beta.15_Linux_arm64.tar.gz"
-    sha256 "bdba9e809b4764234aa4b7558445d243d7d2033f39e55424164739043ed35a27"
+    url "https://github.com/elseano/rundown/releases/download/v0.4.0-beta.16/rundown_0.4.0-beta.16_Linux_arm64.tar.gz"
+    sha256 "b78a1efacd0c75753f27249c85befa3c8ee553d586088b46afd034b8510bb19f"
   end
 
   def install
